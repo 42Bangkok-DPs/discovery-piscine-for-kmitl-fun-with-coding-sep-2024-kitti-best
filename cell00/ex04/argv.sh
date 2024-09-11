0844139703c75fcda77821b762d260985142235d
best <kittibest46@gmail.com>
2024-09-12 00:15:01 +0700

    finish card

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,19 +1,19 @@
 if [ -z $1 ]
+then
+	echo -n "No arguments supplied"
+else
+	if [ ! -z $1 ]
 	then
-		echo -n "No arguments supplied"
-	else
-		if [ ! -z $1 ]
-		then
-			echo -n $1
-		fi
-		if [ ! -z $2 ]
-		then
-			echo
-			echo -n $2
-		fi
-		if [ ! -z $3 ]
-		then
-			echo
-			echo -n $3
-		fi
+		echo -n $1
+	fi
+	if [ ! -z $2 ]
+	then
+		echo
+		echo -n $2
+	fi
+	if [ ! -z $3 ]
+	then
+		echo
+		echo -n $3
+	fi
 fi
